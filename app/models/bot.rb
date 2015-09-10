392e2e82dc7a9b7566eeaae6c1583a25bc67dbda
Carly Jugler <carjug@gmail.com>
2015-09-10 15:08:37 -0700

    Extracts find or create followers out into its own method

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -1,30 +1,32 @@
 class Bot < ActiveRecord::Base
-# going to need to get the twitter handle of the person who has issued the hopeful tweet as well
-# Look at twitter docs for what attributes are available on the Tweet object through the Twitter gem
+  # Need to write a check so bot does not reply to a single tweet more than once.
 
-  def self.find_or_create_followers()
+  def self.find_and_reply()
     @followers = []
 
     CLIENT.followers.collect do |f|
       follower = []
       @user = CLIENT.user(f)
       @tweet = @user.tweet.text
+
       if @tweet.include?("I am hopeful")
-        CLIENT.update(Bot.send_reply_tweet(@user.screen_name), in_reply_to_status_id: @user.id.to_s)
+        CLIENT.update(Bot.send_reply_tweet(@user.screen_name), in_reply_to_status_id: @user.tweet.id)
       end
 
       follower.push(@user.screen_name)
       follower.push(@user.id.to_s)
       @followers.push(follower)
     end
-
-    @followers.each do |follower|
-      Follower.find_or_create_by(name: follower[0], tweet_id: follower[1])
-    end
+    Bot.create_followers(@followers)
   end
 
   def self.send_reply_tweet(name)
     "@" + name + " " + Response.order_by_rand.first.tweet
   end
 
+  def self.create_followers(collection)
+    collection.each do |follower|
+      Follower.find_or_create_by(name: follower[0], tweet_id: follower[1])
+    end
+  end
 end
