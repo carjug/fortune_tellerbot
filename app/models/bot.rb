cbba761a30b6cdf1d63427d309a3720b0d5a0de1
Carly Jugler <carjug@gmail.com>
2015-09-09 23:06:09 -0700

    Adds comments for reminders of what to do with Twitter gem and API calls

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -1,5 +1,8 @@
 class Bot < ActiveRecord::Base
 
+# going to need to get the twitter handle of the person who has issued the hopeful tweet as well
+# Look at twitter docs for what attributes are available on the Tweet object through the Twitter gem
+
   def self.search_words(words)
     CLIENT.search(words, lang: "en").first.text
   end
