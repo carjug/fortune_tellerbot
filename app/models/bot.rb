ec1119748459bf73721569020d31d97f3ac3ac71
Carly Jugler <carjug@gmail.com>
2015-09-10 12:38:58 -0700

    Adds bot controller and new self method for replying to a given tweet

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -3,8 +3,12 @@ class Bot < ActiveRecord::Base
 # going to need to get the twitter handle of the person who has issued the hopeful tweet as well
 # Look at twitter docs for what attributes are available on the Tweet object through the Twitter gem
 
-  def self.search_words(words)
-    CLIENT.search(words, lang: "en").first.text
+  def self.search_hope
+    CLIENT.search("I hope that", lang: "en").first
+  end
+
+  def self.send_reply_tweet(tweet, id)
+    CLIENT.update(tweet, in_reply_to_status_id: id)
   end
 
 end
