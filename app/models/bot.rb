0dba6f81ae893551ddce1587ebfc25c276cecc5f
Carly Jugler <carjug@gmail.com>
2015-09-10 14:02:55 -0700

    Adds validation for uniqueness of follower names. Also weird half working methods for responding to follower's hopeful tweets

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -1,7 +1,15 @@
 class Bot < ActiveRecord::Base
-
 # going to need to get the twitter handle of the person who has issued the hopeful tweet as well
 # Look at twitter docs for what attributes are available on the Tweet object through the Twitter gem
+
+  def self.find_followers()
+   @screen_names = CLIENT.followers.collect do |f|
+      CLIENT.user(f).screen_name
+    end
+    @screen_names.each do |f|
+      Follower.create(name: f)
+    end
+  end
 
   def self.search_hope
     @tweet = CLIENT.search("I hope that", lang: "en").first
@@ -9,8 +17,6 @@ class Bot < ActiveRecord::Base
     @tweet_id = @tweet.id
 
     @tweet_string = @tweet.text
-
-    # Bot.send_reply_tweet(@tweet_id)
   end
 
   def self.send_reply_tweet(id)
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150909233157) do
+ActiveRecord::Schema.define(version: 20150910200848) do
 
   create_table "bots", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "followers", force: :cascade do |t|
+    t.string   "name"
+    t.string   "tweet_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
