d451d5f0921680a2c70ab55261a72a20d8d1da29
Carly Jugler <carjug@gmail.com>
2015-09-13 12:38:42 -0700

    Adds A WORKING BOTTTTTTT YAYYYYYYYYY

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,21 +1,8 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
-
-# Example:
-#
 set :output, {:standard => 'cron.log'}
-# job_type :rake, "cd /Users/cajugler/ada/project_forks/fortune_tellerbot && RAILS_ENV=production bundle exec rake bot:run_bot --silent"
-#
-every '*/2 * * * *' do
-  # command "/usr/bin/some_great_command"
-  # runner "Bot.find_and_reply"
+
+set :environment,'development'
+
+every 2.minutes do
   rake "bot:run_bot"
 end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
 
-# Learn more: http://github.com/javan/whenever
