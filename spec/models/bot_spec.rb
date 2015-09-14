aca4b0094527b80fffcb1ad038f8a65f33c81142
Carly Jugler <carjug@gmail.com>
2015-09-13 17:05:20 -0700

    Adds passing test for knowing about tweets

--- spec/models/bot_spec.rb
+++ spec/models/bot_spec.rb
@@ -1,26 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Bot, type: :model do
-
   context "#create_twitter_posts" do
+    before :each do
+      @client = Twitter::REST::Client.new do |config|
+        config.consumer_key        = ENV['TWITTER_KEY']
+        config.consumer_secret     = ENV['TWITTER_SECRET']
+        config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+        config.access_token_secret = ENV['TWITTER_TOKEN_SECRET']
+      end
+      Tweet.create(tweet_id: "1")
+      Response.create(tweet: "Don't count on it")
+    end
+
     it "returns of list of followers from the API" do
       VCR.use_cassette('get followers of bot') do
-
-        client = Twitter::REST::Client.new do |config|
-          config.consumer_key        = ENV['TWITTER_KEY']
-          config.consumer_secret     = ENV['TWITTER_SECRET']
-          config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
-          config.access_token_secret = ENV['TWITTER_TOKEN_SECRET']
-        end
-        @followers = []
-
-        client.followers.collect do |f|
-          user = client.user(f)
-          @followers.push(user)
+        followers = []
+        @client.followers.collect do |f|
+          user = @client.user(f)
+          followers.push(user)
         end
 
-        expect(@followers.count).to eq 7
+        expect(followers.count).to eq 7
       end
+    end
+
+    it "knows about existing tweets" do
+      @tweets = Tweet.all
+
+      expect(@tweets.count). to eq 1
+    end
+
+    it "generates a response" do
+
     end
   end
 end
